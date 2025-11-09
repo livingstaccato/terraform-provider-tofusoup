84bf257b08234d244af5dc1c00c791f4b3b4dcaf
Tim <code@tim.life>
2025-11-08 19:46:19 -0800

    🔼⚙️ [skip ci] auto-commit

--- tests/data_sources/conftest.py
+++ tests/data_sources/conftest.py
@@ -10,7 +10,7 @@ from typing import Any
 
 import pytest
 from pyvider.resources.context import ResourceContext  # type: ignore
-from tofusoup.registry.models.module import ModuleVersion  # type: ignore
+from tofusoup.registry.models.module import Module, ModuleVersion  # type: ignore
 from tofusoup.registry.models.provider import ProviderPlatform, ProviderVersion  # type: ignore
 
 from tofusoup.tf.components.data_sources.provider_info import ProviderInfoConfig  # type: ignore
@@ -129,6 +129,52 @@ def sample_module_versions() -> list[ModuleVersion]:
             inputs=[],
             outputs=[],
             resources=[],
+        ),
+    ]
+
+
+@pytest.fixture
+def sample_module_search_results() -> list[Module]:
+    """Sample module search results list."""
+    return [
+        Module(
+            id="terraform-aws-modules/vpc/aws",
+            namespace="terraform-aws-modules",
+            name="vpc",
+            provider_name="aws",
+            description="Terraform module to create AWS VPC resources",
+            source_url="https://github.com/terraform-aws-modules/terraform-aws-vpc",
+            downloads=152826752,
+            verified=False,
+            versions=[],
+            latest_version=None,
+            registry_source=None,
+        ),
+        Module(
+            id="terraform-aws-modules/eks/aws",
+            namespace="terraform-aws-modules",
+            name="eks",
+            provider_name="aws",
+            description="Terraform module to create AWS EKS resources",
+            source_url="https://github.com/terraform-aws-modules/terraform-aws-eks",
+            downloads=45628934,
+            verified=True,
+            versions=[],
+            latest_version=None,
+            registry_source=None,
+        ),
+        Module(
+            id="terraform-aws-modules/rds/aws",
+            namespace="terraform-aws-modules",
+            name="rds",
+            provider_name="aws",
+            description="Terraform module to create AWS RDS resources",
+            source_url="https://github.com/terraform-aws-modules/terraform-aws-rds",
+            downloads=23456789,
+            verified=False,
+            versions=[],
+            latest_version=None,
+            registry_source=None,
         ),
     ]
 
