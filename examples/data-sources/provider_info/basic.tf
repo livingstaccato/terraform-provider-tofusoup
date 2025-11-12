d6a77e26d42a6434935035d61ce8c69080b7f5cb
Tim <code@tim.life>
2025-11-11 19:39:25 -0800

    🔼⚙️ [skip ci] auto-commit

--- examples/data-sources/provider_info/basic.tf
+++ examples/data-sources/provider_info/basic.tf
@@ -5,10 +5,10 @@ data "tofusoup_provider_info" "aws_terraform" {
   registry  = "terraform"
 }
 
-# Query AWS provider from OpenTofu registry (note: uses "opentofu" namespace)
-data "tofusoup_provider_info" "aws_opentofu" {
-  namespace = "opentofu"  # OpenTofu uses "opentofu" namespace, not "hashicorp"
-  name      = "aws"
+# Query null provider from OpenTofu registry
+data "tofusoup_provider_info" "null_opentofu" {
+  namespace = "hashicorp"
+  name      = "null"
   registry  = "opentofu"
 }
 
@@ -23,11 +23,11 @@ output "terraform_downloads" {
 }
 
 output "opentofu_version" {
-  description = "Latest AWS provider version from OpenTofu registry"
-  value       = data.tofusoup_provider_info.aws_opentofu.latest_version
+  description = "Latest null provider version from OpenTofu registry"
+  value       = data.tofusoup_provider_info.null_opentofu.latest_version
 }
 
-output "opentofu_source" {
-  description = "Source URL for OpenTofu fork"
-  value       = data.tofusoup_provider_info.aws_opentofu.source_url
+output "opentofu_description" {
+  description = "Description of null provider from OpenTofu registry"
+  value       = data.tofusoup_provider_info.null_opentofu.description
 }
